fb233713532707ee8c1596dcf6564d2d0b4b53bc
Valalol <valentin.esnault92@gmail.com>
2022-05-07 15:43:26 +0200

    Images updated and bug fix install.bat updated

--- install.bat
+++ install.bat
@@ -1,9 +1,12 @@
 TITLE Pyperclip Library install Script
 
 @ECHO OFF
-ECHO Press enter to install the pyperclip library
+ECHO Press enter to install the required libraries
 PAUSE
 pip install pyperclip
+pip install tk
+pip install requests
+pip install webbrowser
 ECHO 
 ECHO Library installed!!
 PAUSE
